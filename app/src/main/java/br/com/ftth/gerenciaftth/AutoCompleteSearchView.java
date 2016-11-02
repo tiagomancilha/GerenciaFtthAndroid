92040f11913659acb502f3d401261ea70172c5fb
tiagomancilha <tiagomancilha@yahoo.com.br>
2016-11-02 18:23:30 -0200

    nova versao de gerenciaftth. Gravacao de marcadores localmente. Com logotipo starweb teste com o henrique

---
+++ app/src/main/java/br/com/ftth/gerenciaftth/AutoCompleteSearchView.java
@@ -0,0 +1,49 @@
+package br.com.ftth.gerenciaftth;
+
+
+import android.content.Context;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v7.widget.SearchView;
+import android.util.AttributeSet;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+
+public class AutoCompleteSearchView extends SearchView {
+
+    private SearchView.SearchAutoComplete mSearchAutoComplete;
+
+    public AutoCompleteSearchView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public AutoCompleteSearchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public void initialize() {
+        mSearchAutoComplete = (android.support.v7.widget.SearchView.SearchAutoComplete) findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        this.setAdapter(null);
+        this.setOnItemClickListener(null);
+    }
+
+    @Override
+    public void setSuggestionsAdapter(CursorAdapter adapter) {
+        // don't let anyone touch this
+    }
+
+    public void setOnItemClickListener(AdapterView.OnItemClickListener listener) {
+        mSearchAutoComplete.setOnItemClickListener(listener);
+    }
+
+    public void setAdapter(ArrayAdapter<?> adapter) {
+        mSearchAutoComplete.setAdapter(adapter);
+    }
+
+    public void setText(String text) {
+        mSearchAutoComplete.setText(text);
+    }
+
+}
